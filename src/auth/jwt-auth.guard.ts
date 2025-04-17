8c2feeb0c75968d579473fb977f063812bd21312
Lee <leesavage09@gmail.com>
2025-04-17 12:41:50 +0300

    added hook into jwt auth guard to log data

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -1,5 +1,34 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  canActivate(context: ExecutionContext) {
+    // Add your custom authentication logic here
+    // for example, call super.logIn(request) to establish a session.
+    return super.canActivate(context);
+  }
+
+  handleRequest(
+    err: any,
+    user: any,
+    info: any,
+    context: ExecutionContext,
+    status?: any,
+  ): any {
+    console.log({
+      function: 'handleRequest',
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      err,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      user,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      info,
+      context,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      status,
+    });
+
+    return user;
+  }
+}
